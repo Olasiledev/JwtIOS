dcfa7e5edab7f8cea46b28f7acd06d50d1e18c15
yourusernameOlasiledevgit config --global user.email email@youremail.comdtech400@gmail.com <oladipupoolasile2@gmail.com>
2024-04-04 01:15:03 -0400

    Initial Commit

---
+++ JwtIOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JwtIOSApp
+//
+//  Created by Oladipupo Olasile on 2024-04-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
